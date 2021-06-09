25d3d3e6ecaa5c6aad7ee66c986fa14cce92d10f
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-06-09 12:23:07 -0400

    Working generate rest tests.

--- commands/generate/generatorController.js
+++ commands/generate/generatorController.js
@@ -1,12 +1,16 @@
 const { generateModel } = require("./models/generateModel");
 const { generateRoutes } = require("./routes/generateRoutes");
+const { generateRestTest } = require("../generate/tests/generateRestTest");
 
 const generatorController = (userInput) => {
   if (userInput[1] === "model" || userInput[1] === "m") {
     generateModel(userInput);
-  }
-  if (userInput[1] === "routes" || userInput[1] === "r") {
+  } else if (userInput[1] === "routes" || userInput[1] === "r") {
     generateRoutes(userInput);
+  } else if (userInput[1] === "tests" || userInput[1] === "t") {
+    generateRestTest(userInput);
+  } else {
+    console.log(`Sorry, that command wasnt recognized`);
   }
 };
 
