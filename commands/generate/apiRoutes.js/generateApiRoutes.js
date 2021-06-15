9445317652f1d420a72768ad706b532f6786ed47
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-06-15 13:29:17 -0400

    progress on generate api routes

--- commands/generate/apiRoutes.js/generateApiRoutes.js
+++ commands/generate/apiRoutes.js/generateApiRoutes.js
@@ -5,11 +5,54 @@ const { existsSync } = require("fs");
 
 const generateApiRoutes = async (userInput) => {
   const modelName = userInput[2];
+
+  const upperCaseFirstLetterModelName = userInput[2];
   if (!modelName) {
     return `no routeName recieved`;
   }
 
-  const indexApiPage = `// blank content for pages index`;
+  const indexApiPage = `
+  import ${upperCaseFirstLetterModelName} from "../../../components/models/${modelName}";
+
+  import { MongoClient } from "mongodb";
+  
+  export default async (req, res) => {
+    if (req.method === "GET") {
+      const client = await MongoClient.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      const db = client.db();
+  
+      const ${modelName}sCollection = await db.collection("${modelName}s");
+  
+      let ${modelName}s = await ${modelName}sCollection.find().toArray();
+  
+      client.close();
+  
+      res.status(200).json(${modelName}s);
+    } else if (req.method === "POST") {
+      const client = await MongoClient.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      const db = client.db();
+  
+      const ${modelName}sCollection = db.collection("${modelName}s");
+  
+      const ${modelName} = await new ${upperCaseFirstLetterModelName}(req.body);
+  
+      let new${upperCaseFirstLetterModelName} = await ${modelName}sCollection.insertOne(${modelName});
+  
+      client.close();
+  
+      res.status(200).json({ new${upperCaseFirstLetterModelName} });
+    } else if (req.method === "DELETE") {
+      res.status(200).json({ name: "John Doe delete" });
+    }
+  };`;
 
   const dynamicApiPage = `// blank dynamic page`;
 
