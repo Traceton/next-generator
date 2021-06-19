13d29e77c88cae949418106993f3bbb7f7c86ec4
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-06-19 16:07:53 -0400

    updated command functionality

--- index.js
+++ index.js
@@ -9,11 +9,32 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question(`Next-generator command: \n`, (answer) => {
-  let userInput = answer.trim().split(" ");
-  if (userInput[0] === "generate" || userInput[0] === "g") {
-    generatorController(userInput);
-  }
+// rl.question(`Next-generator command: \n`, (answer) => {
+//   let userInput = answer.trim().split(" ");
+//   if (userInput[0] === "generate" || userInput[0] === "g") {
+//     generatorController(userInput);
+//   }
 
-  rl.close();
-});
+//   rl.close();
+// });
+
+const recursiveReadline = () => {
+  rl.question("next-generator command: ", function (answer) {
+    let userInput = answer.trim().split(" ");
+    console.log(userInput[0]);
+    switch (userInput[0]) {
+      case "exit":
+        rl.close();
+        break;
+      case "generate":
+        generatorController(userInput);
+        break;
+      case "g":
+        generatorController(userInput);
+        break;
+    }
+    recursiveReadline();
+  });
+};
+
+recursiveReadline();
