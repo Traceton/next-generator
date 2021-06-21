c7ad17196c1d1df33d987cc3480fb2d7d96accf6
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-06-20 23:05:38 -0400

    basic tests written for generatePages.js

--- commands/generate/pages/tests/generatePages.test.js
+++ commands/generate/pages/tests/generatePages.test.js
@@ -11,8 +11,50 @@ describe(`Test generating all page routes for a model with valid input`, () => {
     generatePages(["generate", "p", "car"]);
   });
 
+  it(`"fs.mkdirSync" should be called with "pages,pages/cars" `, () => {
+    expect(fs.mkdirSync).toHaveBeenNthCalledWith(
+      1,
+      `pages`,
+      { recursive: true },
+      expect.any(Function)
+    );
+
+    expect(fs.mkdirSync).toHaveBeenNthCalledWith(
+      2,
+      `pages/cars`,
+      { recursive: true },
+      expect.any(Function)
+    );
+  });
+
   it(` "fs.mkdirSync" to be called 2 times`, () => {
     expect(fs.mkdirSync).toHaveBeenCalledTimes(2);
+  });
+
+  it(`"fs.writeFileSync" should be called with ".env.local,pages/cars/index.js,pages/cars/[carId].js" `, () => {
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      1,
+      `.env.local`,
+      expect.any(String),
+      { recursive: true },
+      expect.any(Function)
+    );
+
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      2,
+      `pages/cars/index.js`,
+      expect.any(String),
+      { recursive: true },
+      expect.any(Function)
+    );
+
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      3,
+      `pages/cars/[carId].js`,
+      expect.any(String),
+      { recursive: true },
+      expect.any(Function)
+    );
   });
 
   it(` "fs.writeFileSync" to be called 3 times`, () => {
