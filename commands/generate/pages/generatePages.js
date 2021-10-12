1fb0dbca157060d76c45d2d63723b97c98a1b20b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-12 00:20:02 -0400

    small progress on index page

--- commands/generate/pages/generatePages.js
+++ commands/generate/pages/generatePages.js
@@ -17,7 +17,7 @@ const generatePages = async (userInput) => {
     modelItems.map((unSplitEntry) => {
         let entry = unSplitEntry.split(":");
         let entryName = entry[0];
-        let modelField = `<h1>${modelName} ${entryName} ---> {${modelName}.${entryName}}</h1>`;
+        let modelField = `<p className="text-lg text-gray-500 truncate">id: {${modelName}.${entryName}}</p>`;
         let jsonBodyField = ` ${entryName}: event.target.${entryName}.value`;
         let formField = `<div className="flex flex-col m-4 p-2 text-center"> \
                     <label htmlFor="${entryName}" className="text-2xl"> \
@@ -73,44 +73,44 @@ const generatePages = async (userInput) => {
         .replace("]", "")
         .replace(/,/g, "");
     const indexPage = `
-export default function ${upperCaseFirstLetterModelName}(props) {
-  return (
-    <div className="bg-white shadow overflow-hidden sm:rounded-md">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">${upperCaseFirstLetterModelName}s</span>
-        </h2>
-      </div>
-      <div>
-      {props.${modelName}s.map((${modelName}) => (
-        <li key={${modelName}._id}>
-        ${finalSchemaItems}
-        </li>
-      ))}
+    <div className="bg-gray-700 h-screen">
+    <div className="p-2">
+      <div className="m-4 p-4 rounded-md bg-gray-400 font-light space-y-8">
+        <div className="flex flex-row">
+          <div className="p-4 m-4">
+            <h3 className="text-4xl leading-6 font-light text-gray-900">${upperCaseFirstLetterModelName}s</h3>
+          </div>
+
+          <div className="m-2 p-2 w-full justify-end">
+            <div className="pt-2 flex justify-end">
+              <Link href="/${modelName}s/create${upperCaseFirstLetterModelName}">
+                <a className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
+                  New
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {props.${modelName}s.map((${modelName}) => (
+            <div
+              key={${modelName}._id}
+              className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+            >
+              <div className="flex-1 min-w-0">
+                <a href={"/${modelName}s/" + ${modelName}._id} className="focus:outline-none">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  ${finalSchemaItems}
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  );
-}
-
-export const getStaticProps = async (context) => {
-  // fetch ${modelName} data from api here
-  const res = await fetch("http://localhost:3000/api/${modelName}s");
-
-  const data = await res.json();
-
-  if (!data) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      ${modelName}s: data.${modelName}s,
-    },
-  };
-};
-  `;
+  </div>
+`;
     const dynamicPage = `
   import { useRouter } from "next/router";
 
