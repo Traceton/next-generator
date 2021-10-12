335f4954989ef3a2da03582848a6373894ed1224
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-12 00:26:29 -0400

    progress on index poage

--- commands/generate/pages/generatePages.ts
+++ commands/generate/pages/generatePages.ts
@@ -136,19 +136,19 @@ export const generatePages = async (userInput: string[]) => {
   const indexPage = `
   import Link from 'next/link'
 
-  export default function Vehicle(props) {
+  export default function ${upperCaseFirstLetterModelName}(props) {
     return (
       <div className="bg-gray-700 h-screen">
         <div className="p-2">
           <div className="m-4 p-4 rounded-md bg-gray-400 font-light space-y-8">
             <div className="flex flex-row">
               <div className="p-4 m-4">
-                <h3 className="text-4xl leading-6 font-light text-gray-900">Vehicles</h3>
+                <h3 className="text-4xl leading-6 font-light text-gray-900">${upperCaseFirstLetterModelName}s</h3>
               </div>
   
               <div className="m-2 p-2 w-full justify-end">
                 <div className="pt-2 flex justify-end">
-                  <Link href="/vehicles/createVehicle">
+                  <Link href="/${modelName}s/create${upperCaseFirstLetterModelName}">
                     <a className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
                       New
                     </a>
@@ -158,18 +158,18 @@ export const generatePages = async (userInput: string[]) => {
             </div>
   
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-              {props.vehicles.map((vehicle) => (
+              {props.${modelName}s.map((${modelName}) => (
                 <div
-                  key={vehicle._id}
+                  key={${modelName}._id}
                   className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                 >
                   <div className="flex-1 min-w-0">
-                    <a href={"/vehicles/" + vehicle._id} className="focus:outline-none">
+                    <a href={"/${modelName}s/" + ${modelName}._id} className="focus:outline-none">
                       <span className="absolute inset-0" aria-hidden="true" />
-                      <p className="text-lg text-gray-500 truncate">id: {vehicle._id}</p>
-                      <p className="text-lg text-gray-500 truncate">year: {vehicle.year}</p>
-                      <p className="text-lg text-gray-500 truncate">make: {vehicle.make}</p>
-                      <p className="text-lg text-gray-500 truncate">model: {vehicle.model}</p>
+                      <p className="text-lg text-gray-500 truncate">id: {${modelName}._id}</p>
+                      <p className="text-lg text-gray-500 truncate">year: {${modelName}.year}</p>
+                      <p className="text-lg text-gray-500 truncate">make: {${modelName}.make}</p>
+                      <p className="text-lg text-gray-500 truncate">model: {${modelName}.model}</p>
                     </a>
                   </div>
                 </div>
@@ -182,8 +182,8 @@ export const generatePages = async (userInput: string[]) => {
   }
   
   export const getStaticProps = async (context) => {
-    // fetch vehicle data from api here
-    const res = await fetch("http://localhost:3000/api/vehicles");
+    // fetch ${modelName} data from api here
+    const res = await fetch("http://localhost:3000/api/${modelName}s");
   
     const data = await res.json();
   
@@ -195,7 +195,7 @@ export const generatePages = async (userInput: string[]) => {
   
     return {
       props: {
-        vehicles: data.vehicles,
+        ${modelName}s: data.${modelName}s,
       },
     };
   };
