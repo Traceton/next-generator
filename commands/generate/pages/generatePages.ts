f62848889e95dfb01d46ea42ce126e74e6a1841e
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-12 00:37:30 -0400

    small change

--- commands/generate/pages/generatePages.ts
+++ commands/generate/pages/generatePages.ts
@@ -216,6 +216,116 @@ export const getStaticProps = async (context) => {
 
   `;
 
+  const newDynamicPage = `
+  import Link from 'next/link'
+import { useRouter } from "next/router";
+
+export default function ${modelName}Details(props) {
+  // router object from next
+  const router = useRouter();
+
+  const ${modelName} = props.${modelName}
+
+  return (
+    <div className="bg-gray-700 h-screen">
+      <div className="p-2">
+        <div className=" m-4 p-4 rounded-md bg-gray-400 font-light space-y-8 ">
+          <form >
+            <div>
+              <h3 className="text-4xl leading-6 font-light text-gray-900">Show ${upperCaseFirstLetterModelName}</h3>
+            </div>
+
+            <div className="text-xl mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="sm:col-span-6">
+                <label htmlFor="year" className="block text-3xl font-light text-gray-700">
+                  year
+                </label>
+                <div className="mt-1 flex rounded-md shadow-sm">
+                  <h1 id="year">{props.${modelName}.year}</h1>
+                </div>
+              </div>
+
+              <div className="sm:col-span-6">
+                <label htmlFor="make" className="block text-3xl font-light text-gray-700">
+                  make
+                </label>
+                <div className="mt-1">
+                  <h1 id="make">{props.${modelName}.make}</h1>
+                </div>
+              </div>
+
+              <div className="sm:col-span-6">
+                <label htmlFor="model" className="block text-3xl font-light text-gray-700">
+                  model
+                </label>
+                <div className="mt-1">
+                  <h1 id="model">{props.${modelName}.model}</h1>
+                </div>
+              </div>
+
+              <div className="m-2 p-2 w-full">
+                <div className="  flex justify-start">
+                  <button
+                    onClick={() => router.back()}
+                    type="button"
+                    className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Back
+                  </button>
+                  <Link href={"/${modelName}s/edit${upperCaseFirstLetterModelName}s/" + ${modelName}._id}>
+                    <a className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
+                      Edit
+                    </a>
+                  </Link>
+                </div>
+              </div>
+
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(\`http://localhost:3000/api/${modelName}s\`);
+
+  const data = await res.json();
+
+  const staticPathParams = [];
+
+  await data.${modelName}s.map((${modelName}) => {
+    staticPathParams.push({ params: { ${modelName}Id: ${modelName}._id } });
+  });
+
+  return {
+    paths: staticPathParams,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch ${modelName} data from api here
+  const ${modelName}Id = context.params.${modelName}Id;
+  const res = await fetch(\`http://localhost:3000/api/${modelName}s/\${${modelName}Id}\`);
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      ${modelName}: data.${modelName},
+    },
+  };
+};
+  `
+
   const createPage = `
   import React from "react";
 
