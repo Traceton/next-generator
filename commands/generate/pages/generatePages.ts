8a8def83eaf385b889d2dc484d19ab73ba78260b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-13 00:25:43 -0400

    small change

--- commands/generate/pages/generatePages.ts
+++ commands/generate/pages/generatePages.ts
@@ -74,7 +74,7 @@ export const generatePages = async (userInput: string[]) => {
     .replace(/,/g, "")
     .replace(/`/g, "")
     .replace(/"/g, "")
-    .replace(/^/g, "\"");
+    .replace(/^/g, `"`);
 
   let finalSchemaItemsForDynamicPage = neWModelSchemaItemsForDynamicPage
     .toString()
