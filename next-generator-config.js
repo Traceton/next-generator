73e54c796b19fa8d52d46bb484b1d0a03f169f25
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-12-07 19:55:35 -0500

    small change

--- next-generator-config.js
+++ next-generator-config.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.acceptedStyles = exports.acceptedDatabases = void 0;
+exports.acceptedStyles = exports.acceptedDatabases = exports.defaultConfig = void 0;
+exports.defaultConfig = { "database": "mongodb", "style": "tailwindcss" };
 exports.acceptedDatabases = ["mongodb"];
 exports.acceptedStyles = ["none", "tailwindcss"];
--- next-generator-config.ts
+++ next-generator-config.ts
@@ -1,3 +1,5 @@
+// default config if one isnt found
+export const defaultConfig = {"database": "mongodb","style": "tailwindcss"}
 // all accepted databases should be listed here
 export const acceptedDatabases = ["mongodb"]
 // all accepted styles should be listed here
