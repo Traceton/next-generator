56b83f79af0df65d77a0c2e98bab61edb1c5d6bc
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-12-24 20:04:24 -0500

    more cleanuop for easier deelopment of new version

--- generateStructureTheory/generateCrud.js
+++ generateStructureTheory/generateCrud.js
@@ -1,12 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateCrud = void 0;
-const generateModel_1 = require("../models/generateModel");
-const generateApiRoutes_1 = require("../apiRoutes.js/generateApiRoutes");
-const generatePages_1 = require("../pages/generatePages");
 const generateCrud = async (userInput) => {
-    await (0, generateModel_1.generateModel)(userInput);
-    await (0, generateApiRoutes_1.generateApiRoutes)(userInput);
-    await (0, generatePages_1.generatePages)(userInput);
 };
 exports.generateCrud = generateCrud;
--- generateStructureTheory/generateCrud.ts
+++ generateStructureTheory/generateCrud.ts
@@ -1,13 +1,8 @@
-
-import { generateModel } from "../models/generateModel"
-import { generateApiRoutes } from "../apiRoutes.js/generateApiRoutes"
-import { generatePages } from "../pages/generatePages"
-
 // generate crud vehicle year:String make:String model:String
 
 export const generateCrud = async (userInput: string[]) => {
-    await generateModel(userInput);
-    await generateApiRoutes(userInput);
-    await generatePages(userInput);
+    // await generateModel(userInput);
+    // await generateApiRoutes(userInput);
+    // await generatePages(userInput);
 };
 
