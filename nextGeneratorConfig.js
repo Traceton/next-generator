46ed30ffb4429d8712a07c33fbe18cf013dee48b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-12-30 13:19:10 -0500

    small text change to fit the nextGenConfig workding properly

--- nextGeneratorConfig.js
+++ nextGeneratorConfig.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.acceptedPageTypes = exports.acceptedDatabases = exports.defaultConfig = void 0;
 exports.defaultConfig = {
-    "pageType": "mongodb",
-    "style": "tailwindcss"
+    "database": "mongodb",
+    "pageType": "tailwindcss"
 };
 exports.acceptedDatabases = ["mongodb"];
 exports.acceptedPageTypes = ["none", "tailwindcss"];
--- nextGeneratorConfig.ts
+++ nextGeneratorConfig.ts
@@ -1,9 +1,9 @@
 // default config if one isnt found
 export const defaultConfig = {
-    "pageType": "mongodb",
-    "style": "tailwindcss"
+    "database": "mongodb",
+    "pageType": "tailwindcss"
 }
 // all accepted databases should be listed here
 export const acceptedDatabases = ["mongodb"]
-// all accepted styles should be listed here
+// all accepted pageType should be listed here
 export const acceptedPageTypes = ["none", "tailwindcss"];
