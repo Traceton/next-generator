ec9ffb5720c37f87416fd8bba3859cc8ed234121
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-08 21:15:09 -0500

    removed css for https://github.com/Traceton/next-generator/issues/6

--- commands/generate/pages/pageTypeNonePages/generateIndex.ts
+++ commands/generate/pages/pageTypeNonePages/generateIndex.ts
@@ -6,23 +6,23 @@ export const generateIndex = (modelName: string, finalSchemaItemsForIndex: strin
     let upperCaseFirstLetterModelName = getUpperCaseFirstLetter(modelName)
 
 
-    const tailwindcssIndex = `
+    const noneIndex = `
     import Link from 'next/link'
 
     export default function ${upperCaseFirstLetterModelName}(props) {
       return (
-        <div className="bg-gray-700 h-screen">
-          <div className="p-2">
-            <div className="m-4 p-4 rounded-md bg-gray-400 font-light space-y-8">
-              <div className="flex flex-row">
-                <div className="p-4 m-4">
-                  <h3 className="text-4xl leading-6 font-light text-gray-900">${upperCaseFirstLetterModelName}s</h3>
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <h3>${upperCaseFirstLetterModelName}s</h3>
                 </div>
 
-                <div className="m-2 p-2 w-full justify-end">
-                  <div className="pt-2 flex justify-end">
+                <div>
+                  <div>
                     <Link href="/${modelName}s/create${upperCaseFirstLetterModelName}">
-                      <a className="m-1 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-xl font-light rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
+                      <a>
                         New
                       </a>
                     </Link>
@@ -30,15 +30,14 @@ export const generateIndex = (modelName: string, finalSchemaItemsForIndex: strin
                 </div>
               </div>
 
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <div>
                 {props.${modelName}s.map((${modelName}) => (
                   <div
-                    key={${modelName}._id}
-                    className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                    key={${modelName}._id}  
                   >
-                    <div className="flex-1 min-w-0">
-                      <a href={"/${modelName}s/" + ${modelName}._id} className="focus:outline-none">
-                        <span className="absolute inset-0" aria-hidden="true" />
+                    <div>
+                      <a href={"/${modelName}s/" + ${modelName}._id}>
+                        <span aria-hidden="true" />
                         ${finalSchemaItemsForIndex}
                       </a>
                     </div>
@@ -77,5 +76,5 @@ export const generateIndex = (modelName: string, finalSchemaItemsForIndex: strin
     };
   `
 
-    return tailwindcssIndex
+    return noneIndex
 }
