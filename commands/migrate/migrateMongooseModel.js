a35f284654bb412f2f014da10da8b66b82d26503
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-10 00:26:31 -0500

    migrateMongooseModel created, copied from generate mongoose model.

---
+++ commands/migrate/migrateMongooseModel.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateMongooseModel = void 0;
+const utils_1 = require("../../utils");
+const fs_1 = require("fs");
+const generateMongooseModel = async (userInput) => {
+    let modelName = userInput[2];
+    let configData = (0, utils_1.readNextConfig)();
+    if (modelName === undefined || modelName === "undefined") {
+        console.log(`must enter a model name`);
+        return `must enter a model name`;
+    }
+    if (userInput.length <= 3) {
+        console.log(`must enter model schema parameters`);
+        return `must enter model schema parameters`;
+    }
+    try {
+        const upperCaseFirstLetterModelName = modelName.charAt(0).toUpperCase() + modelName.slice(1);
+        let modelItems = userInput.slice(3);
+        let neWModelSchemaItems = [];
+        modelItems.map((unSplitEntry) => {
+            let entry = unSplitEntry.split(":");
+            let entryName = entry[0];
+            let entryType = entry[1];
+            let modelField = {
+                [entryName]: {
+                    type: entryType,
+                    required: true,
+                },
+            };
+            let stringField = JSON.stringify(modelField)
+                .replace("{", "")
+                .replace("}", "");
+            neWModelSchemaItems.push(stringField);
+        });
+        let createdOnField = `createdOn: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },`;
+        neWModelSchemaItems.push(createdOnField);
+        let finalSchemaItems = neWModelSchemaItems
+            .toString()
+            .replace("[", "")
+            .replace("]", "")
+            .replace(/"/g, "");
+        let newModel = `const mongoose = require("mongoose"); \n
+
+  const ${modelName}Schema = new mongoose.Schema({
+
+  ${finalSchemaItems}
+
+  }); \n
+
+  module.exports = mongoose.models.${modelName} || mongoose.model("${modelName}", ${modelName}Schema);`;
+        if (!(0, fs_1.existsSync)(`${configData.projectRootPath}components`)) {
+            (0, utils_1.createDirectory)(`${configData.projectRootPath}components`);
+        }
+        if (!(0, fs_1.existsSync)(`${configData.projectRootPath}components/models`)) {
+            (0, utils_1.createDirectory)(`${configData.projectRootPath}components/models`);
+        }
+        (0, utils_1.createFile)(`${configData.projectRootPath}components/models/${upperCaseFirstLetterModelName}.js`, newModel);
+    }
+    catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+exports.generateMongooseModel = generateMongooseModel;
---
+++ commands/migrate/migrateMongooseModel.ts
@@ -0,0 +1,86 @@
+import { createDirectory, createFile, readNextConfig } from "../../utils";
+import { existsSync } from "fs";
+
+// generates a mongoose/mongodb model using userInput. 
+export const generateMongooseModel = async (userInput: string[]) => {
+  let modelName = userInput[2];
+
+  let configData = readNextConfig()
+
+  if (modelName === undefined || modelName === "undefined") {
+    console.log(`must enter a model name`);
+    return `must enter a model name`;
+  }
+
+  if (userInput.length <= 3) {
+    console.log(`must enter model schema parameters`);
+    return `must enter model schema parameters`;
+  }
+
+  try {
+    const upperCaseFirstLetterModelName =
+      modelName.charAt(0).toUpperCase() + modelName.slice(1);
+
+    let modelItems = userInput.slice(3);
+
+    let neWModelSchemaItems = [];
+
+    // maps through each command
+    modelItems.map((unSplitEntry) => {
+      let entry = unSplitEntry.split(":");
+      let entryName = entry[0];
+      let entryType = entry[1];
+
+      let modelField = {
+        [entryName]: {
+          type: entryType,
+          required: true,
+        },
+      };
+
+      let stringField = JSON.stringify(modelField)
+        .replace("{", "")
+        .replace("}", "");
+      neWModelSchemaItems.push(stringField);
+    });
+
+    let createdOnField = `createdOn: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },`;
+    neWModelSchemaItems.push(createdOnField);
+
+    let finalSchemaItems = neWModelSchemaItems
+      .toString()
+      .replace("[", "")
+      .replace("]", "")
+      .replace(/"/g, "");
+
+    let newModel = `const mongoose = require("mongoose"); \n
+
+  const ${modelName}Schema = new mongoose.Schema({
+
+  ${finalSchemaItems}
+
+  }); \n
+
+  module.exports = mongoose.models.${modelName} || mongoose.model("${modelName}", ${modelName}Schema);`;
+
+    if (!existsSync(`${configData.projectRootPath}components`)) {
+       createDirectory(`${configData.projectRootPath}components`);
+    }
+
+    if (!existsSync(`${configData.projectRootPath}components/models`)) {
+       createDirectory(`${configData.projectRootPath}components/models`);
+    }
+
+    createFile(
+      `${configData.projectRootPath}components/models/${upperCaseFirstLetterModelName}.js`,
+      newModel
+    );
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
