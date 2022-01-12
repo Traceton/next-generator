fbd0673b7662ce337834a57677f020f68a825a3d
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-11 22:06:50 -0500

    update help command for - https://github.com/Traceton/next-generator/issues/17

--- commands/help/generateHelp.ts
+++ commands/help/generateHelp.ts
@@ -1,53 +1,51 @@
 // The response the help command will generate if called in the command line interface.
 
- export const generateHelp = async () => {
-    console.log(`***** User Guide ***** \n `);
-    console.log(` Commands:  \n `);
-    console.log(` init - generate a nextGenConfig.json  \n `);
-    console.log(` exit or e - exit next-generator build mode.  \n `);
-    console.log(
-      ` help or h - get help and view the latest commands for next-generator  \n `
-    );
-    console.log(
-      ` generate or g - generate new files based on the commands given next  \n  \n `
-    );
-    console.log(` Generate commands:  \n `);
-    console.log(
-      ` crud or c - generate a mongoose model, all api routes, and all pages for that model. (Create-Read-Update-Delete)   \n `
-    );
-    console.log(
-      ` crud or c - Format : "generate crud your-model-name field-name:data-type field-name:data-type"   \n `
-    );
-    console.log(
-      ` crud or c - Example : "generate crud vehicle year:Number make:String model:String"   \n  \n `
-    );
-    console.log(` model or m - generate a mongoose model.  \n `);
-    console.log(
-      ` model or m - Format : "generate model your-model-name field-name:data-type field-name:data-type"  \n `
-    );
-    console.log(
-      ` model or m - Example : "generate model vehicle year:Number make:String model:String"  \n  \n `
-    );
-    console.log(
-      ` api-routes or a-r - generate next js crud (Create-Read-Update-Delete) api routes.  \n `
-    );
-    console.log(
-      ` api-routes or a-r - Format : "generate api-routes your-model-name field-name:data-type field-name:data-type"  \n `
-    );
-    console.log(
-      ` api-routes or a-r - Example : "generate api-routes vehicle year:Number make:String model:String"  \n  \n `
-    );
-    console.log(
-      ` pages or p - generate next js crud (Create-Read-Update-Delete) pages.  \n `
-    );
-    console.log(
-      ` pages or p - Format : "generate pages your-model-name field-name:data-type field-name:data-type"  \n `
-    );
-    console.log(
-      ` pages or p - Example : "generate pages vehicle year:Number make:String model:String"  \n  \n `
-    );
-  
-    console.log(`***** End of User Guide *****`);
-  };
+export const generateHelp = async () => {
+  console.log(`***** User Guide ***** \n `);
+  console.log(` Commands:  \n `);
+  console.log(`nextGen init - generate a nextGenConfig.json  \n `);
+  console.log(
+    `nextGen help or h - get help and view the latest commands for next-generator  \n `
+  );
+  console.log(
+    `nextGen generate or g - generate new files based on the commands given next  \n  \n `
+  );
+  console.log(` Generate commands:  \n `);
+  console.log(
+    ` crud or c - generate a mongoose model, all api routes, and all pages for that model. (Create-Read-Update-Delete)   \n `
+  );
+  console.log(
+    ` crud or c - Format : "nextGen generate crud your-model-name field-name:data-type field-name:data-type"   \n `
+  );
+  console.log(
+    ` crud or c - Example : "nextGen generate crud vehicle year:Number make:String model:String"   \n  \n `
+  );
+  console.log(` model or m - generate a mongoose model.  \n `);
+  console.log(
+    ` model or m - Format : "nextGen generate model your-model-name field-name:data-type field-name:data-type"  \n `
+  );
+  console.log(
+    ` model or m - Example : "nextGen generate model vehicle year:Number make:String model:String"  \n  \n `
+  );
+  console.log(
+    ` api-routes or a-r - generate next js crud (Create-Read-Update-Delete) api routes.  \n `
+  );
+  console.log(
+    ` api-routes or a-r - Format : "nextGen generate api-routes your-model-name field-name:data-type field-name:data-type"  \n `
+  );
+  console.log(
+    ` api-routes or a-r - Example : "nextGen generate api-routes vehicle year:Number make:String model:String"  \n  \n `
+  );
+  console.log(
+    ` pages or p - generate next js crud (Create-Read-Update-Delete) pages.  \n `
+  );
+  console.log(
+    ` pages or p - Format : "nextGen generate pages your-model-name field-name:data-type field-name:data-type"  \n `
+  );
+  console.log(
+    ` pages or p - Example : "nextGen generate pages vehicle year:Number make:String model:String"  \n  \n `
+  );
 
-  
+  console.log(`***** End of User Guide *****`);
+};
+
