2ca65308ebf9144472d4a07129b2cc0cea85ac48
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-19 16:02:22 -0500

    working on modifying the prisma schema if one is found

---
+++ commands/generate/models/databasePostgresqlModels/generatePostgresqlModel.js
@@ -0,0 +1,87 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateMongooseModel = void 0;
+const utils_1 = require("../../../../utils");
+const fs_1 = require("fs");
+const fs_2 = __importDefault(require("fs"));
+const generateMongooseModel = async (userInput) => {
+    let modelName = userInput[2];
+    let configData = (0, utils_1.readNextConfig)();
+    if (modelName === undefined || modelName === "undefined") {
+        console.log(`must enter a model name`);
+        return `must enter a model name`;
+    }
+    if (userInput.length <= 3) {
+        console.log(`must enter model schema parameters`);
+        return `must enter model schema parameters`;
+    }
+    try {
+        const upperCaseFirstLetterModelName = modelName.charAt(0).toUpperCase() + modelName.slice(1);
+        let modelItems = userInput.slice(3);
+        let neWModelSchemaItems = [];
+        modelItems.map((unSplitEntry) => {
+            let entry = unSplitEntry.split(":");
+            let entryName = entry[0];
+            let entryType = entry[1];
+            let modelField = {
+                [entryName]: {
+                    type: entryType,
+                    required: true,
+                },
+            };
+            let stringField = JSON.stringify(modelField)
+                .replace("{", "")
+                .replace("}", "");
+            neWModelSchemaItems.push(stringField);
+        });
+        let createdOnField = `createdOn: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },`;
+        neWModelSchemaItems.push(createdOnField);
+        let finalSchemaItems = neWModelSchemaItems
+            .toString()
+            .replace("[", "")
+            .replace("]", "")
+            .replace(/"/g, "");
+        let newModel = `const mongoose = require("mongoose"); \n
+  
+    const ${modelName}Schema = new mongoose.Schema({
+  
+    ${finalSchemaItems}
+  
+    }); \n
+  
+    module.exports = mongoose.models.${modelName} || mongoose.model("${modelName}", ${modelName}Schema);`;
+        try {
+            let path = `${configData.projectRootPath}/prisma/schema.prisma`;
+            const rawConfigFile = fs_2.default.readFileSync(path, { encoding: "utf8" });
+            if (!rawConfigFile || rawConfigFile == undefined) {
+                console.log("no prisma schema found, cannot modify");
+            }
+            else {
+                configData = JSON.parse(rawConfigFile);
+                console.log(`prisma schema -> ${configData}`);
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+        if (!(0, fs_1.existsSync)(`${configData.projectRootPath}components`)) {
+            await (0, utils_1.createDirectory)(`${configData.projectRootPath}components`);
+        }
+        if (!(0, fs_1.existsSync)(`${configData.projectRootPath}components/models`)) {
+            await (0, utils_1.createDirectory)(`${configData.projectRootPath}components/models`);
+        }
+        (0, utils_1.createFile)(`${configData.projectRootPath}components/models/${upperCaseFirstLetterModelName}.js`, newModel);
+    }
+    catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+exports.generateMongooseModel = generateMongooseModel;
---
+++ commands/generate/models/databasePostgresqlModels/generatePostgresqlModel.ts
@@ -0,0 +1,102 @@
+import { createDirectory, createFile, readNextConfig } from "../../../../utils";
+import { existsSync} from "fs";
+import fs from "fs";
+
+// generates a mongoose/mongodb model using userInput. 
+export const generateMongooseModel = async (userInput: string[]) => {
+    let modelName = userInput[2];
+  
+    let configData = readNextConfig()
+  
+    if (modelName === undefined || modelName === "undefined") {
+      console.log(`must enter a model name`);
+      return `must enter a model name`;
+    }
+  
+    if (userInput.length <= 3) {
+      console.log(`must enter model schema parameters`);
+      return `must enter model schema parameters`;
+    }
+  
+    try {
+      const upperCaseFirstLetterModelName =
+        modelName.charAt(0).toUpperCase() + modelName.slice(1);
+  
+      let modelItems = userInput.slice(3);
+  
+      let neWModelSchemaItems = [];
+  
+      // maps through each command
+      modelItems.map((unSplitEntry) => {
+        let entry = unSplitEntry.split(":");
+        let entryName = entry[0];
+        let entryType = entry[1];
+  
+        let modelField = {
+          [entryName]: {
+            type: entryType,
+            required: true,
+          },
+        };
+  
+        let stringField = JSON.stringify(modelField)
+          .replace("{", "")
+          .replace("}", "");
+        neWModelSchemaItems.push(stringField);
+      });
+  
+      let createdOnField = `createdOn: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },`;
+      neWModelSchemaItems.push(createdOnField);
+  
+      let finalSchemaItems = neWModelSchemaItems
+        .toString()
+        .replace("[", "")
+        .replace("]", "")
+        .replace(/"/g, "");
+  
+      let newModel = `const mongoose = require("mongoose"); \n
+  
+    const ${modelName}Schema = new mongoose.Schema({
+  
+    ${finalSchemaItems}
+  
+    }); \n
+  
+    module.exports = mongoose.models.${modelName} || mongoose.model("${modelName}", ${modelName}Schema);`;
+
+    // try to modify the Prisma schema
+    try {
+        let path = `${configData.projectRootPath}/prisma/schema.prisma`
+        
+        const rawConfigFile = fs.readFileSync(path, { encoding: "utf8" });
+        if (!rawConfigFile || rawConfigFile == undefined) {
+            console.log("no prisma schema found, cannot modify")
+          } else {
+            configData = JSON.parse(rawConfigFile)
+            console.log(`prisma schema -> ${configData}`)
+          }
+    } catch (error) {
+        console.log(error)
+    }
+  
+      if (!existsSync(`${configData.projectRootPath}components`)) {
+        await createDirectory(`${configData.projectRootPath}components`);
+      }
+  
+      if (!existsSync(`${configData.projectRootPath}components/models`)) {
+        await createDirectory(`${configData.projectRootPath}components/models`);
+      }
+  
+      createFile(
+        `${configData.projectRootPath}components/models/${upperCaseFirstLetterModelName}.js`,
+        newModel
+      );
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
