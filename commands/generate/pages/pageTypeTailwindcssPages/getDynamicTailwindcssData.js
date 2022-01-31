8cfa976644448032c6aeec982496080ea1a6ae2f
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-01-31 16:05:38 -0500

    updated generateCreate

--- commands/generate/pages/pageTypeTailwindcssPages/getDynamicTailwindcssData.js
+++ commands/generate/pages/pageTypeTailwindcssPages/getDynamicTailwindcssData.js
@@ -15,20 +15,25 @@ const getDynamicTailwindcssData = (modelName, modelItems) => {
         let modelFieldForIndex = `<td className=^px-2 py-4 whitespace-nowrap text-md text-gray-200^>{${modelName}.${entryName}}</td>`;
         let modelFieldForDynamicPage = `<div className=^sm:col-span-6^><label htmlFor=^${entryName}^ className=^block text-3xl font-light text-gray-700^>${entryName}</label><div className=^mt-1 flex rounded-md shadow-sm^><h1 id=^year^>{props.${modelName}.${entryName}}</h1></div></div>`;
         let jsonBodyField = ` ${entryName}: event.target.${entryName}.value`;
-        let formField = `<div className="sm:col-span-6">
-                          <label htmlFor="${entryName}" className="block text-3xl font-light text-gray-700">
-                            ${entryName}
-                          </label>
-                          <div className="mt-1 flex rounded-md shadow-sm">
-                            <input
-                              type="text"
-                              name="${entryName}"
-                              id="${entryName}"
-                              autoComplete="${entryName}"
-                              className="shadow-sm  border border-transparent block h-10 w-full text-xl font-light sm:text-sm border border-gray-300 rounded-md"
-                            />
-                          </div>
-                        </div>`;
+        let formField = `
+    <div>
+    <label
+      htmlFor="${entryName}"
+      className="block text-sm font-medium text-white"
+    >
+    ${entryName}
+    </label>
+    <div className="mt-1">
+      <input
+        id="${entryName}"
+        name="${entryName}"
+        type="${entryName}"
+        autoComplete="${entryName}"
+        required
+        className="appearance-none block w-full px-3 py-2 bg-gray-700 border-0 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
+      />
+    </div>
+  </div>`;
         let editFormField = `<div className="sm:col-span-6">
         <label htmlFor="${entryName}" className="block text-3xl font-light text-gray-700">
           ${entryName}
