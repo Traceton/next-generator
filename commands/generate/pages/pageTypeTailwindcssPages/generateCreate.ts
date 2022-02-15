bed58177512c6c8f649281648201d75f5fd90f52
Traceton Timmerman <traceton.timmerman@gmail.com>
2022-02-14 20:03:51 -0500

    fix for bg-white background and bg-gray background both being generated

--- commands/generate/pages/pageTypeTailwindcssPages/generateCreate.ts
+++ commands/generate/pages/pageTypeTailwindcssPages/generateCreate.ts
@@ -40,7 +40,7 @@ export default function Create${upperCaseFirstLetterModelName}() {
         </div>
 
         <div className=" mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-gray-800 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" onSubmit={createNew${upperCaseFirstLetterModelName}}>
               ${finalFormFieldItems}
               <div className="flex flex-row ">
